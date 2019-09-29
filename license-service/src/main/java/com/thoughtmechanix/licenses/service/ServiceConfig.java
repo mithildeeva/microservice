deb2f1e4342c7bea7b4acc17868bbd9ef4f25372
mithil.d <mithil.d@media.net>
2019-09-29 15:36:35 +0530

    moved from file based config to git based

--- license-service/src/main/java/com/thoughtmechanix/licenses/service/ServiceConfig.java
+++ license-service/src/main/java/com/thoughtmechanix/licenses/service/ServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceConfig {
-    @Value("${example.property}")
+    @Value("${spring.application.name}")
     @Getter
     private String exampleProperty;
 }
